f1968570133bdb4fd6e073fe7c198a912da0cffa
paolo-jorge-soriano <182842652+paolo-jorge-soriano@users.noreply.github.com>
2025-07-16 22:12:27 +0800

    add form view

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const path = require("node:path");
 const express = require("express");
 const app = express();
-
 const PORT = 8080;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Middleware
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
 
+// Routing
 const indexRouter = require("./routes/index.js");
 app.use(indexRouter);
 
+// Run the server
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,14 @@ router.get("/", (req, res) => {
   res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
+router.get("/new", (req, res) => {
+  res.render("form", { title: "Add a message" });
+});
+
+router.post("/new", (req, res) => {
+  const { messageText, messageUser } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
+});
+
 module.exports = router;
